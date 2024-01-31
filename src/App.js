70aef41803cc584a175a46e8bc9b9093d400f4ae
bleartdemaku <bd47165@ubt-uni.net>
2024-01-31 13:46:54 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
