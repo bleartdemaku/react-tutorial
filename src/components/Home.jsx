c0fb34ce956ac2526bc594ad8a143afc3f1649ab
bleartdemaku <bd47165@ubt-uni.net>
2024-02-01 21:27:39 +0100

    lists

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("bleart");
-
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("jon");
-    setAge("30");
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website!",
+      body: "lorem ipsum...",
+      author: "bleart",
+      id: 1,
+    },
+    { title: "Welcome!", body: "lorem ipsum...", author: "bleart", id: 2 },
+    {
+      title: "Web dev top tips!",
+      body: "lorem ipsum...",
+      author: "bleart",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>home page</h2>
-      <p>
-        {name} is {age} years old!
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
