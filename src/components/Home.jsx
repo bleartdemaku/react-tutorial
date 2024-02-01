1eb40ced7f619eb19942280f295ec21371225168
bleartdemaku <bd47165@ubt-uni.net>
2024-02-01 14:39:54 +0100

    click events

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log(name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>home page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("bleart", e);
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
