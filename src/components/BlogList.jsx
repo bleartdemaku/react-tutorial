29c6a6c1789a1de646dcba0c11d63c69a9db1321
bleartdemaku <bd47165@ubt-uni.net>
2024-02-02 13:06:22 +0100

    conditional loading message

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,6 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
-  //   const blogs = props.blogs;
-  //   const title = props.title;
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -8,7 +6,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,32 +3,26 @@ import BlogList from "../components/BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsLoading(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      {blogs && (
-        <BlogList
-          blogs={blogs}
-          title="All Blogs!"
-          handleDelete={handleDelete}
-        />
-      )}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
