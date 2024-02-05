cbe45ba4171b7c8cfdb99d793bd72473b4ea17ac
bleartdemaku <bd47165@ubt-uni.net>
2024-02-05 14:18:54 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import CreateBlog from "./components/CreateBlog";
+import NoPage from "./components/NoPage";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="create" element={<CreateBlog />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
